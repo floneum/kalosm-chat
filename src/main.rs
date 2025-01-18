9d1a210b8abf33e0f1a424dcddf996c734451ff4
Evan Almloff <evanalmloff@gmail.com>
2025-01-18 17:19:45 -0600

    update kalosm and dioxus to the latest versions

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,41 @@
 #![allow(non_snake_case)]
 
-use dioxus_router::prelude::*;
-
-use dioxus::{prelude::*, html::input_data::keyboard_types::Key};
-use dioxus_signals::{use_signal, Signal};
+use dioxus::{html::input_data::keyboard_types::Key, prelude::*, CapturedError};
 use kalosm::language::*;
 
 fn main() {
-    dioxus_desktop::launch_cfg(
-        app,
-        dioxus_desktop::Config::new()
-            .with_custom_head(r#"<link rel="stylesheet" href="public/tailwind.css">"#.to_string()),
-    );
+    launch(app);
 }
 
-fn app(cx: Scope) -> Element {
-    render! {
-        Router::<Route> {}
+fn app() -> Element {
+    rsx! {
+        document::Link {
+            rel: "stylesheet",
+            href: asset!("public/tailwind.css"),
+        }
+        document::Link {
+            rel: "stylesheet",
+            href: asset!("public/loading.css"),
+        }
+        ErrorBoundary {
+            handle_error: |error| rsx! {
+                div {
+                    class: "flex flex-col h-screen bg-slate-300",
+                    "{error:#?}"
+                }
+            },
+            SuspenseBoundary {
+                fallback: |_| rsx! {
+                    div {
+                        class: "w-screen h-screen flex flex-col items-center justify-center",
+                        div {
+                            class: "spinner",
+                        }
+                    }
+                },
+                Router::<Route> {}
+            }
+        }
     }
 }
 
@@ -25,17 +44,15 @@ enum Route {
     #[route("/")]
     Setup {},
     #[route("/chat/:assistant_description")]
-    Home {
-        assistant_description: String,
-    },
+    Home { assistant_description: String },
 }
 
 #[component]
-fn Setup(cx: Scope) -> Element {
-    let assistant_description = use_signal(cx, String::new);
-    let navigator = use_navigator(cx);
+fn Setup() -> Element {
+    let mut assistant_description = use_signal(String::new);
+    let navigator = use_navigator();
 
-    render! {
+    rsx! {
         div {
             class: "flex flex-col h-screen bg-slate-300",
 
@@ -55,7 +72,7 @@ fn Setup(cx: Scope) -> Element {
                         placeholder: "Type a description...",
                         value: "{assistant_description}",
                         oninput: move |event| {
-                            assistant_description.set(event.value.clone())
+                            assistant_description.set(event.value())
                         },
                         onkeydown: move |event| {
                             if event.key() == Key::Enter {
@@ -87,25 +104,35 @@ fn Setup(cx: Scope) -> Element {
 }
 
 #[component]
-fn Home(cx: Scope, assistant_description: String) -> Element {
-    let current_message = use_signal(cx, String::new);
-    let messages: Signal<Vec<Signal<Message>>> = use_signal(cx, Vec::new);
-    let assistant_responding = use_signal(cx, || false);
-    let model = cx.use_hook(Llama::new_chat);
-    let chat = use_signal(cx, || {
-        Chat::builder(model).with_system_prompt(assistant_description.clone()).build()
+fn Home(assistant_description: ReadOnlySignal<String>) -> Element {
+    let mut current_message = use_signal(String::new);
+    let mut messages: Signal<Vec<MessageState>> = use_signal(Vec::new);
+    let mut assistant_responding = use_signal(|| false);
+    let model = use_resource(|| async move {
+        Llama::builder()
+            .with_source(LlamaSource::qwen_2_5_0_5b_instruct())
+            .build()
+            .await
+    })
+    .suspend()?;
+    let mut chat: Signal<Result<Chat<Llama>, CapturedError>> = use_signal(move || {
+        let read = model.read();
+        match &*read {
+            Ok(model) => Ok(model.chat().with_system_prompt(assistant_description())),
+            Err(e) => Err(CapturedError::from_display(e.to_string())),
+        }
     });
 
-    render! {
+    rsx! {
         div {
             class: "flex flex-col h-screen bg-slate-300",
 
             div {
                 class: "flex flex-col flex-1 p-4 space-y-4 overflow-y-auto",
 
-                for message in messages().iter().copied() {
+                for message in messages.read().iter().cloned() {
                     Message {
-                        message: message,
+                        message,
                     }
                 }
 
@@ -117,35 +144,38 @@ fn Home(cx: Scope, assistant_description: String) -> Element {
                         placeholder: "Type a message...",
                         value: "{current_message}",
                         oninput: move |event| {
-                            if !*assistant_responding() {
-                                current_message.set(event.value.clone())
+                            if !assistant_responding() {
+                                current_message.set(event.value())
                             }
                         },
                         onkeydown: move |event| {
-                            if !*assistant_responding() && event.key() == Key::Enter {
-                                let mut current_message = current_message.write();
-                                let mut messages = messages.write();
-                                messages.push(Signal::new(Message {
-                                    user: User::User,
-                                    text: current_message.clone()
-                                }));
+                            if !assistant_responding() && event.key() == Key::Enter {
+                                let current_message = current_message.take();
                                 let final_message = current_message.clone();
-                                assistant_responding.set(true);
-                                let assistant_response = Signal::new(Message {
-                                    user: User::Assistant,
-                                    text: String::new(),
-                                });
-                                messages.push(assistant_response);
-                                cx.spawn(async move {
-                                    let mut chat = chat.write();
-                                    if let Ok(mut stream) = chat.add_message(final_message).await {
+                                {
+                                    let mut messages_mut = messages.write();
+                                    messages_mut.push(MessageState {
+                                        user: User::User,
+                                        text: current_message
+                                    });
+                                    assistant_responding.set(true);
+                                    let assistant_response = MessageState {
+                                        user: User::Assistant,
+                                        text: String::new(),
+                                    };
+                                    messages_mut.push(assistant_response);
+                                }
+                                spawn(async move {
+                                    if let Ok(chat) = &mut *chat.write() {
+                                        let mut stream = chat.add_message(final_message);
                                         while let Some(new_text) = stream.next().await {
-                                            assistant_response.write().text += &new_text;
+                                            let mut messages = messages.write();
+                                            let Some(last_message) = messages.last_mut() else { break };
+                                            last_message.text += &new_text;
                                         }
                                     }
                                     assistant_responding.set(false);
                                 });
-                                current_message.clear();
                             }
                         },
                     }
@@ -171,37 +201,34 @@ impl User {
 }
 
 #[derive(PartialEq, Clone)]
-struct Message {
+struct MessageState {
     user: User,
     text: String,
 }
 
 #[component]
-fn Message(cx: Scope, message: Signal<Message>) -> Element {
+fn Message(message: ReadOnlySignal<MessageState>) -> Element {
     let message = message();
-    let align = if message.user == User::Assistant {
-        "self-start"
-    } else {
-        "self-end"
-    };
     let text = &message.text;
     let assistant_placeholder = message.user == User::Assistant && text.is_empty();
-    let text = if assistant_placeholder {
-        "Thinking..."
-    } else {
-        text
-    };
 
-    let text_color = if assistant_placeholder {
-        "text-gray-400"
-    } else {
-        ""
-    };
-    render! {
+    rsx! {
         div {
-            class: "w-2/3 p-2 bg-white rounded-lg shadow-md {align} {text_color}",
+            class: "w-2/3 p-2 bg-white rounded-lg shadow-md",
+            class: if message.user == User::Assistant {
+                "self-start"
+            } else {
+                "self-end"
+            },
+            class: if assistant_placeholder {
+                "text-gray-400"
+            },
             background_color: message.user.background_color(),
-            "{text}"
+            if assistant_placeholder {
+                "Thinking..."
+            } else {
+                "{text}"
+            }
         }
     }
 }
